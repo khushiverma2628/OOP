7aac2df311cdf894295fb4f9007685b062b8f5a7
khushiverma2628 <khushi943142@gmail.com>
2026-01-21 17:13:43 +0530

    Add files via upload

---
+++ Practical7.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+public class Practical7{
+	public static void main(String[] args){
+	Scanner sc = new Scanner(System.in);
+	
+
+	System.out.println("Enter The 3 Sides Of Triangle: ");
+	System.out.println("A: ");
+	int a = sc.nextInt();
+	System.out.println("B: ");
+	int b = sc.nextInt();
+	System.out.println("C: ");
+	int c = sc.nextInt();
+	
+		if(a+b > c && b+c > a && a+c > b){
+			int s = ((a+b+c)/2);
+			double area = Math.sqrt(s * (s-a) * (s-b) * (s-c));
+			
+			System.out.print("The Area Of the Triangle: " + area);
+		}
+		else{
+			System.out.print("The Given Side Does Not Made Triangle.");
+		}
+	
+	}
+
+}
