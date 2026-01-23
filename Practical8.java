24baa8e7a5663a5e61fa8b13bb51ec18d00ac735
khushiverma2628 <khushi943142@gmail.com>
2026-01-23 08:58:55 +0530

    Add files via upload

---
+++ Practical8.java
@@ -0,0 +1,13 @@
+public class Practical8{
+	public static void main(String[] args){
+		
+		
+			int a = Integer.parseInt(args[0]);
+			int b = Integer.parseInt(args[1]);
+			
+			int sum = a+b;
+			int mul = a*b;
+			System.out.println("Sum : "+sum);
+	}
+
+}
