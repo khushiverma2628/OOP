cfed857241b815cd9eb3e0f75deddfc8715194c0
khushiverma2628 <khushi943142@gmail.com>
2026-03-11 15:59:12 +0530

    Add files via upload

---
+++ practical17.java
@@ -0,0 +1,38 @@
+class University
+  static int totalStudent;
+   static string universityName;
+   
+ static 
+{
+ University name = "saffrony instutied of college";
+total student = 0;
+
+  System.out.println("static block executed university");
+
+  {
+     System.out.print("Intance block");
+    
+}
+   static int get totalStudent();
+{
+ return totalStudent;
+
+}
+  university()
+ total student++;
+System.out.println("Construted is Executed.");
+
+
+
+  public static void main(String[] args){
+System.out.println("")
+  University s1 = new.university(1);
+University s2 = new.university(2);
+University s3 = new.university(3);
+
+System.out.println("in total student:" get total student);
+
+}  
+     
+}
+
